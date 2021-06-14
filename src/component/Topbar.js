ec96a976a07b508acc107c9606c7e34f7fd8b073
ddoshi2021 <ddoshi2021@gmail.com>
2021-06-13 18:01:30 -0700

    initial commit

---
+++ src/component/Topbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+
+function Topbar() {
+    return (
+        <div className="top">
+            <div className="topRight"> 
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+                <i className="topIcon fab fa-pinterest-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+            </div>
+
+            <div className="topCenter">
+                    <ul className="topList">
+                        
+                        <li className="topListItem">
+                            Home
+                        </li>
+                        <li className="topListItem">
+                            About
+                        </li>
+                        <li className="topListItem">
+                            Contact
+                        </li>
+                        <li className="topListItem">
+                            Write
+                        </li>
+                </ul>
+            </div>
+
+            <div className="topLeft">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/2873473/pexels-photo-2873473.jpeg?cs=srgb&dl=pexels-david-bartus-2873473.jpg&fm=jpg" 
+              alt="topimage"
+            />
+            <i className="topSearchIcon fas fa-search"></i>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Topbar
