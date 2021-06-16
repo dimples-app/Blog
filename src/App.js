87bb444d8798da67c5f0967f779b7593a466aa67
ddoshi2021 <ddoshi2021@gmail.com>
2021-06-16 14:52:05 -0700

    Add page and component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./App.css"
 import Topbar from "./component/TopBar/Topbar";
 import Header from "./component/Header/Header"
+import SideBar from "./component/SideBar/SideBar";
 import Home from  "./pages/Home"
+import SinglePage from "./pages/SinglePage";
+import Write from "./pages/Write";
 
 function App() {
   return <>
-  <Topbar />
-    <Home />
-
-
-
-
+    <Topbar />
+    <Write />
   </>;
 }
 
