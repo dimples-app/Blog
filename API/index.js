165faed4b385681ec80165df9586a05bbc3222e2
ddoshi2021 <ddoshi2021@gmail.com>
2021-06-19 08:51:02 -0700

    Add hashing

--- API/index.js
+++ API/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv= require("dotenv");
+const mongoose = require('mongoose');
+const authRoute = require("./routes/auth")
 
+
+dotenv.config();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true})
+    .then(console.log("Connected to MongoDB"))
+    .catch( (error) => console.log(error));
+
+    app.use("/api/auth",authRoute)
 app.listen("5000", () => {
     console.log("Listening to 5000")
 })
--- API/routes/auth.js
+++ API/routes/auth.js
@@ -1,2 +1,25 @@
-const express = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const User = require("../model/User")
+const bcrypt = require('bcrypt');
+
+
+router.post("/register", async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashPass = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashPass
+    
+        })
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+
+})
+
+module.exports = router;
