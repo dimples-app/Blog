77e2c544373f2b81e75b1c7d8fc762a12cdcb776
ddoshi2021 <ddoshi2021@gmail.com>
2021-06-19 10:39:52 -0700

    Add login and register route

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../model/User")
 const bcrypt = require('bcrypt');
 
-
+// Post for Register
 router.post("/register", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
@@ -20,6 +20,25 @@ router.post("/register", async (req, res) => {
         res.status(500).json(error);
     }
 
+})
+
+// post for login
+router.post("/login", async(req, res) => {
+    try{
+        // match/validate username
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong Credential")
+
+        // match/validate password
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong Credential")
+
+        const {password, ...others} = user._doc
+
+        res.status(200).json(others);
+    } catch(error) {
+        res.status(500).json(error);
+    }
 })
 
 module.exports = router;
