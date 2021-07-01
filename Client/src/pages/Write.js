67c0d594c29f092923e4adab56558e805b3c4082
dimples <13650125+dimples-app@users.noreply.github.com>
2021-07-01 13:27:19 -0700

    Add Context/ hooks/handler

--- Client/src/pages/Write.js
+++ Client/src/pages/Write.js
@@ -1,40 +1,75 @@
 import React from 'react'
+import { useContext, useState } from "react";
+import axios from "axios";
+import {Context} from "../context/Context";
 
 function Write() {
-    return (
-        <div className="write">
-        <img
-          className="writeImg"
-          src="https://images.pexels.com/photos/1766838/pexels-photo-1766838.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt=""
-        />
-        <form className="writeForm">
-          <div className="writeFormGroup">
-            <label htmlFor="fileInput">
-              <i className="writeIcon fas fa-plus"></i>
-            </label>
-            <input id="fileInput" type="file" style={{ display: "none" }} />
-            <input
-              className="writeInput"
-              placeholder="Title"
-              type="text"
-              autoFocus={true}
-            />
-          </div>
-          <div className="writeFormGroup">
-            <textarea
-              className="writeInput writeText"
-              placeholder="Tell your story..."
-              type="text"
-              autoFocus={true}
-            />
-          </div>
-          <button className="writeSubmit" type="submit">
-            Publish
-          </button>
-        </form>
-      </div>
-    )
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    if (file) {
+      const data =new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (err) {}
+  };
+  return (
+    <div className="write">
+      {file && (
+        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+      )}
+      <form className="writeForm" onSubmit={handleSubmit}>
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+            onChange={e=>setTitle(e.target.value)}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+            onChange={e=>setDesc(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  );
+      
 }
 
 export default Write
