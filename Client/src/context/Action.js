536740a4bcef8471a42f8158c3e30563d7f2f6bc
dimples <13650125+dimples-app@users.noreply.github.com>
2021-07-01 13:19:38 -0700

    Add more actions

--- Client/src/context/Action.js
+++ Client/src/context/Action.js
@@ -1,18 +1,20 @@
 export const LoginStart = (userCredentials) => ({
-    type: "LOGIN_START",
-  });
-  
-  export const LoginSuccess = (user) => ({
-    type: "LOGIN_SUCCESS",
-    payload: user,
-  });
-  
-  export const LoginFailure = () => ({
-    type: "LOGIN_FAILURE",
-  });
-  
-  export const Logout = () => ({
-    type: "LOGOUT",
-  });
-  
-  
+  type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user) => ({
+  type: "LOGIN_SUCCESS",
+  payload: user,
+});
+
+export const LoginFailure = () => ({
+  type: "LOGIN_FAILURE",
+});
+
+export const Logout = () => ({
+  type: "LOGOUT",
+});
+
+export const UpdateStart = (userCredentials) => ({
+  type: "UPDATE_START",
+});
