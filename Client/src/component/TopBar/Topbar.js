8433c7c8138a3ba386605da535b943bd581a6519
dimples <13650125+dimples-app@users.noreply.github.com>
2021-07-01 13:18:22 -0700

    Add context

--- Client/src/component/TopBar/Topbar.js
+++ Client/src/component/TopBar/Topbar.js
@@ -1,58 +1,57 @@
 import React from 'react';
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "./Topbar.css";
 
 
 function Topbar() {
-    const user = false;
-    return (
-        <div className="top">
-            <div className="topRight"> 
-                <i className="topIcon fab fa-facebook-square"></i>
-                <i className="topIcon fab fa-instagram-square"></i>
-                <i className="topIcon fab fa-pinterest-square"></i>
-                <i className="topIcon fab fa-twitter-square"></i>
-            </div>
+  const { user, dispatch } = useContext(Context);
+  const singPostPhoto = "http://localhost:5000/images/"
 
-            <div className="topCenter">
-                    <ul className="topList">
-                        
-                        <li className="topListItem">
-                           <Link className="link" to="/">
-                               Home
-                            </Link> 
-                        </li>
-                        <li className="topListItem">
-                            <Link className="link" to="/">
-                                About
-                            </Link>
-                        </li>
-                        <li className="topListItem">
-                            Contact
-                        </li>
-                        <li className="topListItem">
-                            <Link className="link" to="/write">
-                                Write
-                            </Link>  
-                        </li>
-                        {
-                        user && 
-                        <li className="topListItem">
-                            Logout
-                            </li>
-                        }
-                </ul>
-            </div>
-        
-            <div className="topRight">
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
         {user ? (
-          <Link className="link" to="/settings">
-            <img
-              className="topImg"
-              src="https://images.pexels.com/photos/2873473/pexels-photo-2873473.jpeg?cs=srgb&dl=pexels-david-bartus-2873473.jpg&fm=jpg"
-              alt=""
-            />
+          <Link to="/settings">
+            <img className="topImg" src={singPostPhoto+user.profilePic} alt="" />
           </Link>
         ) : (
           <ul className="topList">
@@ -68,12 +67,10 @@ function Topbar() {
             </li>
           </ul>
         )}
-
-            <i className="topSearchIcon fas fa-search"></i>
-        </div>
-            
-        </div>
-    )
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
 }
 
 export default Topbar
