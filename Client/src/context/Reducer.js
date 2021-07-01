4b8b07d6c9be47f87c43177a117c539b6ffc35d9
dimples <13650125+dimples-app@users.noreply.github.com>
2021-07-01 13:20:27 -0700

    Add more action to reducer

--- Client/src/context/Reducer.js
+++ Client/src/context/Reducer.js
@@ -1,26 +1,49 @@
 const Reducer = (state, action) => {
-    switch (action.type) {
-      case "LOGIN_START":
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: false,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        isFetching: false,
+        error: true,
+      };
+      case "UPDATE_START":
         return {
-          user: null,
-          isFetching: true,
-          error: false,
+          ...state,
+          isFetching:true
         };
-      case "LOGIN_SUCCESS":
+      case "UPDATE_SUCCESS":
         return {
           user: action.payload,
           isFetching: false,
           error: false,
         };
-      case "LOGIN_FAILURE":
+      case "UPDATE_FAILURE":
         return {
-          user: null,
+          user: state.user,
           isFetching: false,
           error: true,
         };
-      default:
-        return state;
-    }
-  };
-  
-  export default Reducer;
+    case "LOGOUT":
+      return {
+        user: null,
+        isFetching: false,
+        error: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
